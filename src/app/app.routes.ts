28a95383a7334c4fafd9206f2284fef35bce7040
Álvaro Grajau Rodrigues <alvaro.rodrigues@dbserver.com.br>
2025-05-23 10:48:17 -0300

    Componente inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PomodoroComponent } from './pages/pomodoro/pomodoro.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pomodoro', component: PomodoroComponent },
+  { path: '**', redirectTo: '' }
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+   imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  userName: string = '';
+
+  constructor(private router: Router) {}
+
+  goToPomodoro() {
+    localStorage.setItem('userName', this.userName);
+    this.router.navigate(['/pomodoro']);
+  }
 
 }
